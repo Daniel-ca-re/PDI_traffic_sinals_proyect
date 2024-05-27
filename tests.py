1c6cc8a6b9db00c04b75ce9b4e085c7aaf069fe9
Daniel-ca-re <daniel.canor1@udea.edu.co>
2024-05-27 14:09:40 -0500

    3
    finalizado

--- tests.py
+++ tests.py
@@ -1,32 +1,68 @@
 import matplotlib.pyplot as plt
 from ipywidgets import interact, FloatSlider
+import numpy as np
 
-def plot_square(a, b, c, d):
+
+def plot_square(ang, a_x, a_y, s_x, s_y):
     """
     Plot a square given its corner coordinates (a, b, c, d)
     """
-    x = [a[0], b[0], c[0], d[0], a[0]]
-    y = [a[1], b[1], c[1], d[1], a[1]]
-    plt.plot(x, y, 'r-')  # 'r-' for red line
+
+
+
+    matriz_rotacion = np.array([[np.cos(ang), -np.sin(ang)],
+                                [np.sin(ang), np.cos(ang)]])
+    
+    # Matriz de escalado
+    matriz_escala = np.array([[a_x, 0],
+                               [0, a_y]])
+    
+    # Multiplicar las matrices de rotación y escalado
+    rot = np.dot(matriz_rotacion, matriz_escala)
+
+    sheer = np.array([[1, s_x],
+                        [s_y, 1]])
+
+    # Realizar la multiplicación de las matrices
+    resultado_matriz = np.dot(sheer,rot)
+    
+    x = [0, 1, 0, -1, 0]
+    y = [0, 1, 2, 1, 0]
+    tx = [0, 0, 1, 1, 0]
+    ty = [0, 1, 1, 0, 0]
+    for i in range(0,len(x)):
+        vector = np.array([x[i], y[i]])
+
+        # Multiplicar el vector por la matriz resultante
+        resultado_vector = np.dot(resultado_matriz, vector)
+        tx[i]=resultado_vector[0]
+        ty[i]=resultado_vector[1]
+
+    plt.figure(figsize=(8, 8))
+    plt.plot(x, y, 'r-', label='Cuadrado original')
+    plt.plot(tx, ty, 'b-', label='Cuadrado transformado')
+
+    # Establecer los límites de los ejes para que siempre sea un cuadrado
+    plt.xlim(-6, 6)
+    plt.ylim(-6, 6)
+
+    # Etiquetas y leyenda
     plt.xlabel('X')
     plt.ylabel('Y')
-    plt.title('Cuadrado')
+    plt.title('Rotación con escalado')
+    plt.legend()
+
+    # Mostrar el gráfico
     plt.grid(True)
-    plt.axis('equal')
     plt.show()
 
 def main():
-    # Initial square coordinates
-    a = [0, 0]
-    b = [0, 1]
-    c = [1, 1]
-    d = [1, 0]
-    
     interact(plot_square,
-             a=FloatSlider(min=-5, max=5, step=0.1, value=a[1]),
-             b=FloatSlider(min=-5, max=5, step=0.1, value=b[1]),
-             c=FloatSlider(min=-5, max=5, step=0.1, value=c[1]),
-             d=FloatSlider(min=-5, max=5, step=0.1, value=d[1]))
+             ang=FloatSlider(min=-np.pi, max=np.pi, step=0.01, value=0),
+             a_x=FloatSlider(min=0, max=5, step=0.01, value=1),
+             a_y=FloatSlider(min=0, max=5, step=0.01, value=1),
+             s_x=FloatSlider(min=-1, max=1, step=0.01, value=0),
+             s_y=FloatSlider(min=-1, max=1, step=0.01, value=0))
 
 if __name__ == "__main__":
     main()
